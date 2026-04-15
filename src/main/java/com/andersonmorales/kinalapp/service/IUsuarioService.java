817b4a0e67d38075512940865a4aa57bf825a33e
amorales-2024057 <amorales-2024057@kinal.edu.gt>
2026-04-15 00:23:25 -0600

    fix: Se agregó lo que resta para poder iniciar sesión

--- src/main/java/com/andersonmorales/kinalapp/service/IUsuarioService.java
+++ src/main/java/com/andersonmorales/kinalapp/service/IUsuarioService.java
@@ -1,7 +1,6 @@
 package com.andersonmorales.kinalapp.service;
 
 import com.andersonmorales.kinalapp.entity.Usuario;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -9,7 +8,11 @@ public interface IUsuarioService {
     List<Usuario> listarTodos();
     Usuario guardar(Usuario usuario);
     Optional<Usuario> buscarPorCodigo(long codigo);
+    Optional<Usuario> buscarPorUsername(String username);
+    Optional<Usuario> buscarPorEmail(String email);
     Optional<Usuario> buscarPorEstado(int estado);
+    Usuario login(String username, String password);
+    boolean registrar(Usuario usuario);
     Usuario actualizar(long codigo, Usuario usuario);
     void eliminar(long codigo);
     boolean existePorCodigo(long codigo);
