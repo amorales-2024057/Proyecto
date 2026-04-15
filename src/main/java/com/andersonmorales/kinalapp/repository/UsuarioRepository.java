f047ac90e0fd0e4b5b15f2ffd3111ba4b210aa36
amorales-2024057 <amorales-2024057@kinal.edu.gt>
2026-04-15 00:19:36 -0600

    fix: Se agregó métodos para poder iniciar seción

--- src/main/java/com/andersonmorales/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/andersonmorales/kinalapp/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package com.andersonmorales.kinalapp.repository;
 
 import com.andersonmorales.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsername(String username);
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
